b4283d5a59a5e87e349a784dd9cc34d9858800d2
youyeon11 <yurry45@naver.com>
2025-08-28 17:51:56 +0900

    del: 불필요한 변수 삭제

--- app/core/config.py
+++ app/core/config.py
@@ -2,14 +2,13 @@ from pydantic_settings import BaseSettings
 import os
 
 class Settings(BaseSettings):
-    REDIS_URL: str = os.environ.get("REDIS_SERVER_URL", "redis://localhost:6379")
+    REDIS_URL: str = os.environ.get("REDIS_URL", "redis://localhost:6379")
     S3_REGION: str = os.environ.get("S3_REGION", "ap-northeast-2")
-    S3_ENDPOINT_URL: str | None = os.environ.get("S3_ENDPOINT_URL")
     BUCKET_NAME: str = os.environ.get("BUCKET_NAME")
 
-    STREAM_JOB: str = "image.jobs"
-    STREAM_RESULT: str = "image.results"
-    GROUP_NAME: str = "fastapi-workers"
+    STREAM_JOB: str = "image.jobs" # SpringBoot에서 job 발행 (FastAPI에서 listen)
+    STREAM_RESULT: str = "image.results"  # FastAPI에서 결과 발행 (SpringBoot에서 listen)
+    GROUP_NAME: str = "fastapi-workers" # FastAPI Consumer group name
     CONSUMER_NAME: str = "consumer-1"
 
 
