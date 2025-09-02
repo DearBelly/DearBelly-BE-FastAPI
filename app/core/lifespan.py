a47a0569bb8aad117acfb44fb2cac3a6896021c8
kite_U <yurry45@naver.com>
2025-09-03 03:54:02 +0900

    fix: 하나의 공통된 AsyncRedis를 사용하도록 설정

--- app/core/lifespan.py
+++ app/core/lifespan.py
@@ -4,12 +4,14 @@ from contextlib import asynccontextmanager
 import redis.asyncio as redis
 from fastapi import FastAPI
 
+from app.worker.redis_client import redis_client
 from app.core.config import settings
 from app.worker.worker import JobWorker
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
-    redis_client = redis.from_url(settings.REDIS_URL, decode_responses=True)
+    limiter = asyncio.to_thread.current_default_thread_limiter()
+    limiter.total_tokens = 50
 
     try:
         await redis_client.xgroup_create(
--- app/worker/redis_client.py
+++ app/worker/redis_client.py
@@ -1,10 +1,95 @@
+import asyncio
 import redis
+
+from redis.asyncio import Redis as AsyncRedis
 from pydantic import BaseModel, Field
-from typing import Any, Dict
+from typing import Any, Dict, Optional, List
 from app.core.config import settings
 
-redis_client = redis.asyncio.from_url(settings.REDIS_URL, decode_responses=True)
-
+# Redis Stream Definition
 class PublishRequest(BaseModel):
-    stream: str = Field(default=settings.JOB_STREAM, description="Redis Stream Job name")
+    stream: str = Field(default=settings.STREAM_JOB, description="Redis Stream Job name")
     payload: Dict[str, Any]
+
+class RedisStreamClient:
+
+    def __init__(self):
+        self.redis_client = AsyncRedis.from_url(
+            url=settings.REDIS_URL,
+            decode_responses=True,
+        )
+
+    @classmethod
+    def init(cls):
+        broker = cls()
+        return broker
+
+    # Fast API 에서 Publish
+    async def xadd(self, stream_name: str, fields: Dict[str, Any]) -> str:
+        await self.redis_client.xadd(stream_name, fields)
+
+    # Group 단위로 읽어오기
+    async def xreadgroup(
+            self,
+            group_name: str,
+            consumer_name: str,
+            stream_name: str,
+            count: Optional[int] = None,
+            block: Optional[int] = None,
+            id: str = ">",
+    ) -> List[tuple]:
+        try:
+            # Create the consumer group ( 존재하지 않을 때 )
+            self.redis_client.xgroup_create(stream_name, group_name, id="$", mkstream=True)
+        except redis.exceptions.ResponseError as e:
+            # 이미 존재할 때
+            if "BUSYGROUP" not in str(e):
+                raise e
+
+        response = self.redis_client.xreadgroup(
+            groupname=group_name,
+            consumername=consumer_name,
+            streams={stream_name: id},
+            count=count,
+            block=block,
+        )
+        return response
+
+    # Consumer 처리 완료
+    def xack(self, stream_name: str, group_name: str, message_ids: List[str]) -> int:
+        return self.redis_client.xack(stream_name, group_name, *message_ids)
+
+    # 완료 시 삭제
+    def xack_and_del(self, stream_name: str, group_name: str, message_ids: List[str]) -> int:
+
+        acked_count = self.redis_client.xack(stream_name, group_name, *message_ids)
+
+        # XACK가 성공하면 스트림에서 해당 메시지를 삭제 (XDEL)
+        if acked_count > 0:
+            self.redis_client.xdel(stream_name, *message_ids)
+
+        return acked_count
+
+
+    # 메시지 재처리 지원
+    def xclaim(
+            self,
+            stream_name: str,
+            group_name: str,
+            consumer_name: str,
+            min_idle_time: int,
+            message_ids: List[str],
+    ) -> List[tuple]:
+
+        return self.redis_client.xclaim(
+            name=stream_name,
+            groupname=group_name,
+            consumername=consumer_name,
+            min_idle_time=min_idle_time,
+            message_ids=message_ids,
+        )
+
+    def close(self):
+        self.redis_client.disconnect()
+
+redis_client = RedisStreamClient.init()
