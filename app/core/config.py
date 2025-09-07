bb4eb0a55360411601d79188e34f01fa8e24dc86
kite_U <yurry45@naver.com>
2025-09-07 17:25:04 +0900

    feat: open AI API kye 추가

--- app/core/config.py
+++ app/core/config.py
@@ -5,14 +5,11 @@ from dotenv import load_dotenv
 load_dotenv()
 
 redis_url = os.getenv("REDIS_SERVER_URL")
-bucket_name = os.getenv("BUCKET_NAME")
-s3_region = os.getenv("S3_REGION")
-
+oepnai_key = os.getenv("OPENAI_API_KEY")
 
 class Settings(BaseSettings):
     REDIS_URL: str = redis_url
-    S3_REGION: str = bucket_name
-    BUCKET_NAME: str = s3_region
+    OPENAI_KEY: str = oepnai_key
 
     STREAM_JOB: str = "image.jobs" # SpringBoot에서 job 발행 (FastAPI에서 listen)
     STREAM_RESULT: str = "image.results"  # FastAPI에서 결과 발행 (SpringBoot에서 listen)
