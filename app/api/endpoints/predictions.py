43cb041e63d10c734afe1b0047c1dbbc7c0bba2d
kite_U <yurry45@naver.com>
2025-09-07 15:34:51 +0900

    fix: Message 작성 및 필드 수정

--- app/api/endpoints/predictions.py
+++ app/api/endpoints/predictions.py
@@ -4,12 +4,16 @@ from app.core.config import settings
 import redis.asyncio as redis
 import uuid
 from datetime import datetime
+import json
 
 router = APIRouter()
 
 def get_redis_client(request: Request) -> redis.Redis:
     return request.app.state.redis_client
 
+"""
+테스트를 위한 임시적인 API
+"""
 @router.post("/predict", status_code=202)
 async def create_prediction_job(job_request: JobRequest, redis_client: redis.Redis = Depends(get_redis_client)):
     correlationId = str(uuid.uuid4())
@@ -22,9 +26,17 @@ async def create_prediction_job(job_request: JobRequest, redis_client: redis.Red
         ttlSec=3600,
     )
 
-    await redis_client.xadd(
+    # 타입에 맞도록 넣어주기
+    correlationId = job.correlationId
+    payload = json.dumps(job.dict())
+    print("분석 결과 발행 시작...")
+    entry_id = await redis_client.xadd(
         settings.STREAM_JOB,
-        {"json": job.model_dump_json()},
+        {
+            "type": "image_results",
+            "payload": payload,
+            "correlationId": correlationId,
+        },
         maxlen=10_000,
         approximate=True,
     )
